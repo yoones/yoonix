7170e7d154ef1161d58ffc0ca647af10ad8941b0
Younes SERRAJ <younes.serraj@gmail.com>
2016-11-04 17:13:57 +0100

    linux-pam: pam aware program - fixed service name and added security check (_username == NULL)

--- linux-pam/pam-aware-program/main.c
+++ linux-pam/pam-aware-program/main.c
@@ -16,10 +16,11 @@ authenticate_user(char **username)
   int ret;
   const void *_username;
 
-  if ((ret = pam_start("my_check_user", NULL, &conv, &pamh)) != PAM_SUCCESS
+  if ((ret = pam_start("login", NULL, &conv, &pamh)) != PAM_SUCCESS
       || (ret = pam_authenticate(pamh, 0)) != PAM_SUCCESS
       || (ret = pam_acct_mgmt(pamh, 0)) != PAM_SUCCESS
-      || (ret = pam_get_item(pamh, PAM_USER, &_username)) != PAM_SUCCESS)
+      || (ret = pam_get_item(pamh, PAM_USER, &_username)) != PAM_SUCCESS
+      || _username == NULL)
     {
       fprintf(stderr, "Error: failed to authenticate user\n");
       pam_end(pamh, ret);
